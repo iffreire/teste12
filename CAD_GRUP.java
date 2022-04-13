584a33e2ef03daa7f552ca46f865611b7a6e94dc
iffreire <103075740+iffreire@users.noreply.github.com>
2022-04-13 20:16:17 -0300

    Add files via upload

--- CAD_GRUP.java
+++ CAD_GRUP.java
@@ -2,10 +2,23 @@ package CADASTRAMENTO;
 import java.util.Scanner;
 
 public class CAD_GRUP{
-
+	public static void main(String[] args) {	
+		Scanner input = new Scanner (System.in);
 	String descgrup;
 	int codgrup;
+		
+	String confirme;
+	boolean conf_me = false;
+	System.out.print("Digite o Codigo do Grupo..: "  );
+	 codgrup   = input.nextInt();
+	 System.out.println();
+	 System.out.print("Digite o Nome do Grupo....: ");
+	 descgrup  = input.next();
+	 System.out.println();
+	 System.out.println();
+	 System.out.print("___________________________________________ ");
+	 System.out.println();
+	 while (!conf_me) {
 	
-
-	
+}
 }
--- CAD_SETOR.java
+++ CAD_SETOR.java
@@ -2,7 +2,30 @@ package CADASTRAMENTO;
 import java.util.Scanner;
 public class CAD_SETOR {
 
+	public static void main(String[] args) {	
+	Scanner input = new Scanner (System.in);
 	String descsetor;
 	int codsetor;
-	
+	String confirme;
+	boolean conf_me = false;
+	System.out.print("Digite o Codigo do Setor..: "  );
+	 codsetor   = input.nextInt();
+	 System.out.println();
+	 System.out.print("Digite o Nome do Setor....: ");
+	 descsetor  = input.next();
+	 System.out.println();
+	 System.out.println();
+	 System.out.print("___________________________________________ ");
+	 System.out.println();
+	 while (!conf_me) {
+     
+	 System.out.print("Salvar (S/N)....: ");
+	 confirme  = input.next();
+	  if (confirme.equalsIgnoreCase ("S")) {
+		System.out.println("Estou aqui"); 
+		conf_me = true;
+	  }
+	  
+	}
+	}
 }
